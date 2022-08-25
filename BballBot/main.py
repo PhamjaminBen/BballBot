bb27c4e72152812bd05a6f0d84ee480e9fd73349
Ben Pham <ben58630@gmail.com>
2022-08-24 17:12:54 -0700

    Pruned on message auto generator

--- BballBot/main.py
+++ BballBot/main.py
@@ -22,22 +22,5 @@ client.load_extension("commands.leaders")
 async def on_ready():
     await client.change_presence(activity = discord.Game(name = "Westbrick construction simulator", type = "playing"))
     print(f'{client.user} has connected to Discord!')
-    
-@client.event
-async def on_message(m: discord.Message):
-    if "lebron" in m.content.lower() and m.author.discriminator != "8178":
-        await m.channel.send(m.content.replace("lebron","LeBum").replace("LeBron","LeBum").replace("Lebron","LeBum"))
-    if "zion" in m.content.lower() and m.author.discriminator != "8178":
-        await m.channel.send(m.content.replace("zion","ZiObese").replace("Zion","ZiObese"))
-    if "curry" in m.content.lower() and m.author.discriminator != "8178":
-        await m.channel.send(m.content.replace("Curry","CurFraud").replace("curry","CurFraud"))
-    if "jokic" in m.content.lower() and m.author.discriminator != "8178":
-        await m.channel.send("If you didn't know, he grew up in a serbian warzone, and his two brothers are buff and can beat you up!")
-    if "metu" in m.content.lower() and m.author.discriminator != "8178":
-        await m.channel.send("ChiMickey Metu")
-    if "chimezie" in m.content.lower() and m.author.discriminator != "8178":
-        await m.channel.send("ChiMickey Metu")
-    if "mezie" in m.content.lower() and m.author.discriminator != "8178":
-        await m.channel.send("ChiMickey Metu")
 
 client.run(token)
